4797fe2694ea67513e206a53e65f2413a066af80
Enzo Lormont <enzo.lormont@etudiant.univ-lr.fr>
2024-02-21 15:36:32 +0100

    Upgrade to PHP 8.1

--- CodeCollidersBasicUserBundle.php
+++ CodeCollidersBasicUserBundle.php
@@ -5,11 +5,12 @@ namespace CodeColliders\BasicUserBundle;
 
 
 use CodeColliders\BasicUserBundle\DependencyInjection\CodeCollidersBasicUserExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CodeCollidersBasicUserBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new CodeCollidersBasicUserExtension();
